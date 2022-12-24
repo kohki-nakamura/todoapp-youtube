6303f529428baf790e18b147962ce62634956fae
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2022-12-25 00:42:30 +0900

    htmlとcssを整えるところまで

---
+++ src/components/InputForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const InputForm = () => {
+  return (
+    <div className="inputForm">
+      <form>
+        <input type="text" />
+        <button>
+          <i className="fa-solid fa-plus"></i>
+        </button>
+      </form>
+    </div>
+  )
+}
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+export const TodoList = () => {
+  return (
+    <div className="todoList">
+      <div className="todos">
+        <div className="todo">
+          <div className="todoText">
+            <span>プログラミング</span>
+          </div>
+          <div className="icons">
+            <button>
+              <i className="fa-solid fa-check"></i>
+            </button>
+            <button>
+              <i className="fa-solid fa-trash"></i>
+            </button>
+          </div>
+        </div>
+
+        <div className="todo">
+          <div className="todoText">
+            <span>プログラミング</span>
+          </div>
+          <div className="icons">
+            <button>
+              <i className="fa-solid fa-check"></i>
+            </button>
+            <button>
+              <i className="fa-solid fa-trash"></i>
+            </button>
+          </div>
+        </div>
+
+        <div className="todo">
+          <div className="todoText">
+            <span>プログラミング</span>
+          </div>
+          <div className="icons">
+            <button>
+              <i className="fa-solid fa-check"></i>
+            </button>
+            <button>
+              <i className="fa-solid fa-trash"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
