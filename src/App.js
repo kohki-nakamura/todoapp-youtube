f3a3881f79f441861d63648d8837b85f6939e198
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2022-12-25 15:58:42 +0900

    タスク追加

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import { Title } from './components/Title';
 import { InputForm } from './components/InputForm';
 import { TodoList } from './components/TodoList';
+import { useState } from 'react';
 
 function App() {
+  const [taskList, setTaskList] = useState([]);
+
   return (
     <div className="body">
       <Title />
-      <InputForm />
-      <TodoList />
+      <InputForm taskList={taskList} setTaskList={setTaskList} />
+      <TodoList taskList={taskList} setTaskList={setTaskList}  />
     </div>
   );
 }
--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export const InputForm = () => {
+export const InputForm = ( {taskList, setTaskList} ) => {
+  const [inputText, setInputText] = useState("");
+
+  const handleChange = (e) => {
+    setInputText(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (inputText === "") return;
+
+    setTaskList([
+        ...taskList,
+        {
+          id: taskList.length,
+          text: inputText
+        }
+      ]);
+
+    setInputText("");
+  }
+
   return (
     <div className="inputForm">
-      <form>
-        <input type="text" />
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleChange} value={inputText} />
         <button>
           <i className="fa-solid fa-plus"></i>
         </button>
